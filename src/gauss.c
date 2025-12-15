c149830fa22cdb4cb6e01b32d5548553afb4212c
Anonim4school <01206836@pw.edu.pl>
2025-12-15 20:36:56 +0100

    Dodanie zerowania elementow pod pivotem

--- src/gauss.c
+++ src/gauss.c
@@ -13,7 +13,15 @@ int eliminate(Matrix *mat, Matrix *b){
 		return 1;
 	}
 	else{
+				for(int i = k+1;i<n;i++){
+			double wspolczynnik = mat->data[i][k] / pivot;
 
+			for(int j = k;j<n;j++){
+				mat->data[i][j] -= wspolczynnik *mat->data[k][j];
+			}
+			b->data[i][0] -= wspolczynnik * b->data[k][0];
+		}
+	}
 		}
 		return 0;
 	}
