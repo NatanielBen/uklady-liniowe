8300701b3cfa4689fc0bd5325c398d8c0aab1e3c
NatanielBen <01206745@pw.edu.pl>
2025-12-18 14:42:01 +0100

    Implementacja funckji backsubst.c

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,22 +1,38 @@
 #include "../include/backsubst.h"
+#include <stdio.h>
+
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+    int r = mat->r;
+    int c = mat->c;
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+	//Sprawdzenie poprawności rozmiarów macierzy
+    if (r != c || r != b->r || r != x->r) {
+        return 2;
+    }
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+    //Wsteczne podstawienie
+    for (int i = r - 1; i >= 0; i--) {
+        
+        //Sprawdzenie dzielenia przez 0
+        if (mat->data[i][i] == 0.0) {
+            return 1;
+        }
 
-				return 0;
+        double sum = 0.0;
+
+        // Obliczenie sumy dla znanych już wartości x[j]
+        for (int j = i + 1; j < c; j++) {
+            sum += mat->data[i][j] * x->data[j][0];
+        }
+
+        // Obliczenie wartości x[i]
+        x->data[i][0] = (b->data[i][0] - sum) / mat->data[i][i];
+    }
+
+    return 0;
 }
-
-
