1a456f39f8a73dd15cc35f52d239dd4e86e3e16c
Anonim4school <01206836@pw.edu.pl>
2026-01-06 13:40:07 +0100

    Poprawa elimincaji Gaussa (Przeniesienie blednej petli) i sprawdzenie macierzy osobliwej

--- src/gauss.c
+++ src/gauss.c
@@ -1,4 +1,5 @@
 #include "../include/gauss.h"
+#include <math.h> // Potrzebne do fabs()
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -11,52 +12,43 @@ int eliminate(Matrix *mat, Matrix *b){
 	if(n != mat->c || n != b->r || b->c != 1){
 		return 2; 
 	}
-	
-	double max = mat->data[0][0]; 
-	int max_row = 0;
 
-	int i = 0; 
-	while(i < n) {
-		if(mat->data[i][0] > max){
-				max = mat->data[i][0];
-				max_row = i;
-		}
-		i++; 
-	}
-
-	if(max_row != 0){
-		int j = 0; 
-		while(j < n) {
-				double temp = mat->data[0][j];
-				mat->data[0][j] = mat->data[max_row][j];
-				mat->data[max_row][j] = temp;
-				j++; 
-		}
+	for(int k = 0; k < n - 1; k++){
 		
-		double temp_b = b->data[0][0];
-		b->data[0][0] = b->data[max_row][0];
-		b->data[max_row][0] = temp_b;
-	}
+		int max_row = k;
+		double max_val = fabs(mat->data[k][k]);
 
-	for(int k = 0;k < mat->r -1;k++){
-		double pivot = mat->data[k][k];
-		if(pivot == 0){
-			return 1;
-		}
-		else{
-				for(int i = k+1;i<n;i++){
-					double wspolczynnik = mat->data[i][k] / pivot;
-
-					for(int j = k;j<n;j++){
-						mat->data[i][j] -= wspolczynnik *mat->data[k][j];
+		for(int i = k + 1; i < n; i++) {
+			if(fabs(mat->data[i][k]) > max_val){
+				max_val = fabs(mat->data[i][k]);
+				max_row = i;
 					}
-
-					b->data[i][0] -= wspolczynnik * b->data[k][0];
 			}
+		if(max_row != k){
+				double *temp = mat->data[k];
+				mat->data[k] = mat->data[max_row];
+				mat->data[max_row] = temp;
+
+				double *temp_b = b->data[k];
+				b->data[k] = b->data[max_row];
+				b->data[max_row] = temp_b;
+		}
+
+		if(fabs(mat->data[k][k]) == 0){
+			return 1; 
+		}
+
+
+		double pivot = mat->data[k][k];
+
+		for(int i = k + 1; i < n; i++){
+				double wspolczynnik = mat->data[i][k] / pivot;
+
+				for(int j = k; j < n; j++){
+						mat->data[i][j] -= wspolczynnik * mat->data[k][j];
+				}
+				b->data[i][0] -= wspolczynnik * b->data[k][0];
 		}
 	}
 	return 0;
 }
-
-
-
